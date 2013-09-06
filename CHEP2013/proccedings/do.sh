265ef7a716cbf59a26ee76d3724a359b37291679
Ilija Vukotic <ivukotic@cern.ch>
2013-09-06 13:22:49 -0500

    rename

--- CHEP2013/proccedings/do.sh
+++ CHEP2013/proccedings/do.sh
@@ -1,1 +1,1 @@
-N=ivukotic_CHEP2012;latex $N;latex $N; bibtex $N; latex $N;latex $N; dvipdf -sPAPERSIZE=a4 -dPDFSETTINGS=/prepress $N
+N=ivukotic_CHEP2013;latex $N;latex $N; bibtex $N; latex $N;latex $N; dvipdf -sPAPERSIZE=a4 -dPDFSETTINGS=/prepress $N
