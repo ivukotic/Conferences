e0acf5181672a51e299ef3abac0631e57d826e1c
Ilija Vukotic <ivukotic@cern.ch>
2013-10-27 08:00:00 -0500

    clean up

--- CHEP2013/proccedings/do.sh
+++
@@ -1,1 +0,0 @@
-N=ivukotic_CHEP2013;latex $N;latex $N; bibtex $N; latex $N;latex $N; dvipdf -sPAPERSIZE=a4 -dPDFSETTINGS=/prepress $N
--- CHEP2013_FAX/proccedings/do.sh
+++
@@ -1,1 +0,0 @@
-N=FAX_CHEP2013;latex $N;latex $N; bibtex $N; latex $N;latex $N; dvipdf -sPAPERSIZE=a4 -dPDFSETTINGS=/prepress $N
